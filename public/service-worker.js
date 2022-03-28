f86eaba0743d2e4aef4fc9611670f1478c967bd8
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-03-28 00:51:22 -0500

    fix heroku deploy

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,6 @@ const FILES_TO_CACHE = [
     './css/styles.css',
     './js/index.js',
     './js/idb.js',
-    './manifest.json',
     './icons/icon-512x512.png',
     './icons/icon-384x384.png',
     './icons/icon-192x192.png',
